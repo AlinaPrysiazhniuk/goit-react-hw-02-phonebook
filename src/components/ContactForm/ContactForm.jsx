31a8ed1aa33e04e447d9f882c04d46cfb03b4954
Alina Prysiazhnuyk <alnkaprisiazhnuyk@gmail.com>
2023-05-06 22:03:45 +0300

    Update ContactForm.jsx

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -44,7 +44,7 @@ export class ContactForm extends Component {
           name="number"
           value={number}
           onChange={this.handleChange}
-          placeholder="number"
+          placeholder="Number"
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
